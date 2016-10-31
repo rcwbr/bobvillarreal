0428d267b1e48ce0d36c5eb3dc6ef860422e6643
Eric Weber <eric@weber.lol>
2016-10-30 23:21:58 -0700

    add filename matrix to json output

--- ikarus.rb
+++ ikarus.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require "json"
 require "./curtiss_image_process.rb"
 
 IKARUS_VERBOSE = false
@@ -9,19 +10,13 @@ GALLERIES_DATA_FILE = "galleries.yaml"
 GALLERIES_DATA_PATH = "galleries"
 DEFAULT_GALLERY_IMAGE_PATH = "source/images/galleries"
 DEFAULT_GALLERY_THUMB_FOLDER_PATH = "thumbs"
-DEFAULT_GALLERY_TOTAL_WIDTH = 960
-DEFAULT_GALLERY_IMAGES_PER_ROW = 4
-DEFAULT_GALLERY_IMAGE_MARGINS = 10
-DEFAULT_GALLERY_WIDTH_ADJUSTMENT = 2
-DEFAULT_GALLERY_OVERLAY_THUMB_HEIGHT = 100
+DEFAULT_GALLERY_TOTAL_WIDTH = 960  #This is the width (in pixel) which Curtiss will make each row fit within. It also sets a number of style attributes from elsewhere in Middleman, where it is accessed by the config[:TOTAL_WIDTH] variable (see below).
+DEFAULT_GALLERY_IMAGES_PER_ROW = 4 #This is the number of images Curtiss will attempt to place on each row. If the imagesprocessed are not wide enough, it will add to this number until it fills the row. This is also accessed elsewhere in Middleman.
+DEFAULT_GALLERY_IMAGE_MARGINS = 10 #This is the number of pixels Curtiss will leave for margins BETWEEN images. It accounts for no margins around the gallery as a whole. It subtracts the number of pixels for margins times the ACTUAL number of images in the row (can be different than IMAGES_PER_ROW, as discussed above) from the TOTAL_WIDTH, and scales images in each row to fit that calculated width.
+DEFAULT_GALLERY_WIDTH_ADJUSTMENT = 2 #This is a number of pixels left blank on each row by default. Try adjusting this if images are wrapping before the end of their rows. The reason for this wiggle room is because pixel counts must be integers, and when rounding from the calculations used to scale images, there is a possibility of more pixels in a row than the TOTAL_WIDTH.
+DEFAULT_GALLERY_OVERLAY_THUMB_HEIGHT = 100 #This is the height of the list of thumbnails used in the overlay. This is not actually used by the image processing script, but it is the only other cofigurable variable used by the front end.
 
 galleries_info = YAML.load_file("#{IKARUS_DATA_PATH}/#{GALLERIES_DATA_FILE}")
-
-# total_width = 960 #This is the width (in pixel) which Curtiss will make each row fit within. It also sets a number of style attributes from elsewhere in Middleman, where it is accessed by the config[:TOTAL_WIDTH] variable (see below).
-# images_per_row = 4 #This is the number of images Curtiss will attempt to place on each row. If the imagesprocessed are not wide enough, it will add to this number until it fills the row. This is also accessed elsewhere in Middleman.
-# image_margins = 10 #This is the number of pixels Curtiss will leave for margins BETWEEN images. It accounts for no margins around the gallery as a whole. It subtracts the number of pixels for margins times the ACTUAL number of images in the row (can be different than IMAGES_PER_ROW, as discussed above) from the TOTAL_WIDTH, and scales images in each row to fit that calculated width.
-# width_adjustment = 2.0 #This is a number of pixels left blank on each row by default. Try adjusting this if images are wrapping before the end of their rows. The reason for this wiggle room is because pixel counts must be integers, and when rounding from the calculations used to scale images, there is a possibility of more pixels in a row than the TOTAL_WIDTH.
-#overlay_thumb_height = 100 #This is the height of the list of thumbnails used in the overlay. This is not actually used by the image processing script, but it is the only other cofigurable variable used by the front end.
 
 galleries_json_file = File.open(IKARUS_OUTPUT_FILENAME, "w")
 galleries_json_file.write('{"galleries":[')
@@ -64,7 +59,7 @@ for gallery_counter in 0...galleries_info.length
   end
 
   puts "IKARUS: #{gallery_name} | #{gallery_shortname} | #{gallery_image_path} | #{gallery_thumb_folder_path} | #{gallery_total_width} | #{gallery_images_per_row} | #{gallery_image_margins} | #{gallery_width_adjustment} | #{gallery_overlay_thumb_height}" if IKARUS_VERBOSE
-  galleries_json_file.write("{\"name\":\"#{gallery_name}\",\"shortname\":\"#{gallery_shortname}\",\"image_path\":\"#{gallery_image_path}\",\"thumb_folder_path\":\"#{gallery_thumb_folder_path}\",\"total_width\":\"#{gallery_total_width}\",\"images_per_row\":\"#{gallery_images_per_row}\",\"image_margins\":\"#{gallery_image_margins}\",\"width_adjustment\":\"#{gallery_width_adjustment}\",\"overlay_thumb_height\":\"#{gallery_overlay_thumb_height}\"")
+  galleries_json_file.write("{\"name\":\"#{gallery_name}\",\"shortname\":\"#{gallery_shortname}\",\"image_path\":\"#{gallery_image_path}\",\"thumb_folder_path\":\"#{gallery_thumb_folder_path}\",\"total_width\":\"#{gallery_total_width}\",\"images_per_row\":\"#{gallery_images_per_row}\",\"image_margins\":\"#{gallery_image_margins}\",\"width_adjustment\":\"#{gallery_width_adjustment}\",\"overlay_thumb_height\":\"#{gallery_overlay_thumb_height}\",\"filename_matrix\":[")
 
   gallery_image_filename_matrix = []
   gallery_image_filenames = []
@@ -80,15 +75,16 @@ for gallery_counter in 0...galleries_info.length
     print "IKARUS: " if IKARUS_VERBOSE
     for j in 0...gallery_image_filename_matrix[i].length
       print "#{gallery_image_filename_matrix[i][j]} " if IKARUS_VERBOSE
-
     end
     puts "" if IKARUS_VERBOSE
   end
 
+  galleries_json_file.write(gallery_image_filename_matrix.to_json())
+
   if gallery_counter + 1 < galleries_info.length
-    galleries_json_file.write("},")
+    galleries_json_file.write("]},")
   else
-    galleries_json_file.write("}")
+    galleries_json_file.write("]}")
   end
 end
 
