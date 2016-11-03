4f59a6ab41d032fdf70246a65b9f88340aed1a5d
Eric Weber <eric@weber.lol>
2016-11-02 18:33:19 -0700

    simplify config.rb by passing gallery_info variable directly to gallery template and layout

--- config.rb
+++ config.rb
@@ -27,23 +27,11 @@ SITE_ROOT_PATH = "../"
 CONFIG_VERBOSE = true
 galleries = JSON.parse(File.read("ikarus_galleries.json"));
 
-#galleries = [
-#	{ "name" => "Ecuador - Chimborazo", "shortname" => "ecuador", "data-file" => "data/galleries/ecuador.yaml" },
-#	{ "name" => "Bolivia - Illimani", "shortname" => "bolivia", "data-file" => "data/galleries/bolivia.yaml" }
-#]
 galleries.each do |gallery_info|
 	puts "GALLERY: Name: #{gallery_info["name"]} | Short Name: #{gallery_info["shortname"]} | Images Path: #{gallery_info["images_path"]} | Thumbs Folder Path: #{gallery_info["thumbs_folder_path"]} | Total Width: #{gallery_info["total_width"]} | Images Per Row: #{gallery_info["images_per_row"]} | Image Margins: #{gallery_info["image_margins"]} | Width Adjustment: #{gallery_info["width_adjustment"]} | Overlay Thumbnail Height: #{gallery_info["overlay_thumb_height"]}" if CONFIG_VERBOSE
 	puts "GALLERY: Image Filename Matrix: #{gallery_info["image_filename_matrix"]}" if CONFIG_VERBOSE
-	#, :locals => { , :filename_matrix => filename_matrix, :gallery_folder => gallery_folder, :thumbs_folder => thumbs_folder, :total_width => total_width, :images_per_row => images_per_row, :image_margins => image_margins, :width_adjustment => width_adjustment, :overlay_thumb_height => overlay_thumb_height}
-	filename_matrix = gallery_info["image_filename_matrix"]
-	gallery_folder = "#{SITE_ROOT_PATH}#{gallery_info["images_path"]}" #"galleries/ecuador"
-	thumbs_folder	= gallery_info["thumbs_folder_path"]
-	total_width = gallery_info["total_width"]
-	images_per_row = gallery_info["images_per_row"]
-	image_margins = gallery_info["image_margins"]
-	width_adjustment = gallery_info["width_adjustment"]
-	overlay_thumb_height = gallery_info["overlay_thumb_height"]
-	proxy "/galleries/#{gallery_info["shortname"]}/index.html", "/templates/gallery.html", :locals => { :gallery_info => gallery_info, :filename_matrix => filename_matrix, :gallery_folder => gallery_folder, :thumbs_folder => thumbs_folder, :total_width => total_width, :images_per_row => images_per_row, :image_margins => image_margins, :width_adjustment => width_adjustment, :overlay_thumb_height => overlay_thumb_height }, :ignore => true
+	gallery_info["images_path"] = "#{SITE_ROOT_PATH}#{gallery_info["images_path"]}"
+	proxy "/galleries/#{gallery_info["shortname"]}/index.html", "/templates/gallery.html", :locals => { :gallery_info => gallery_info }, :ignore => true
 end
 
 ###
