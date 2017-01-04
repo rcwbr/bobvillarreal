bf02df5deebf66b12103572dfe5d263370baa719
Eric Weber <eric@weber.lol>
2017-01-03 22:28:13 -0800

    restyle home page; add passage index page

--- config.rb
+++ config.rb
@@ -27,35 +27,29 @@ activate :directory_indexes
 GALLERY_SITE_ROOT_PATH = "../../"
 CONFIG_VERBOSE = false
 galleries = JSON.parse(File.read("data/ikarus_galleries.json"))
-
 galleries.each do |gallery_info|
 	puts "GALLERY: Name: #{gallery_info["name"]} | Short Name: #{gallery_info["shortname"]} | Images Path: #{gallery_info["images_path"]} | Thumbs Folder Path: #{gallery_info["thumbs_folder_path"]} | Total Width: #{gallery_info["total_width"]} | Images Per Row: #{gallery_info["images_per_row"]} | Image Margins: #{gallery_info["image_margins"]} | Width Adjustment: #{gallery_info["width_adjustment"]} | Overlay Thumbnail Height: #{gallery_info["overlay_thumb_height"]}" if CONFIG_VERBOSE
 	puts "GALLERY: Image Filename Matrix: #{gallery_info["image_matrix"]}" if CONFIG_VERBOSE
 	gallery_info["images_path"] = "#{GALLERY_SITE_ROOT_PATH}#{gallery_info["images_path"]}"
 	proxy "/gallery/#{gallery_info["shortname"]}/index.html", "/templates/gallery.html", :locals => { :gallery_info => gallery_info }, :ignore => true
 end
-
 proxy "/galleries/index.html", "/templates/galleries.html", :locals => { :galleries => galleries }, :ignore => true
 
 passages = JSON.parse(File.read("data/burgess_passages.json"))
-
 passages.each do |passages_info|
 	puts "PASSAGES: Name: #{passages_info["name"]} | Short Name: #{passages_info["shortname"]}" if CONFIG_VERBOSE
 	puts "PASSAGES: Passages Array: #{passages_info["passages"]}" if CONFIG_VERBOSE
 	proxy "/passages/#{passages_info["shortname"]}/index.html", "/templates/passages.html", :locals => { :passages_info => passages_info }, :ignore => true
 end
+proxy "/passages/index.html", "/templates/passages_index.html", :locals => { :passages => passages }, :ignore => true
 
 chapters = JSON.parse(File.read("data/gallaudet_chapters.json"))
-
 chapters.each do |chapter_info|
 	puts "CHAPTER: Name: #{chapter_info["name"]}" if CONFIG_VERBOSE
 	if chapter_info["ikarus_data"]
 		chapter_info["ikarus_data"]["images_path"] = "#{GALLERY_SITE_ROOT_PATH}#{chapter_info["ikarus_data"]["images_path"]}"
 	end
 	proxy "/chapter/#{chapter_info["shortname"]}/index.html", "/templates/chapter.html", :locals => { :chapter_info => chapter_info, :gallery_info => chapter_info["ikarus_data"], :passages_info => chapter_info["burgess_data"] }, :ignore => true
-	#else
-	#	proxy "/chapter/#{chapter_info["shortname"]}/index.html", "/templates/chapter.html", :locals => { :chapter_info => chapter_info }, :ignore => true
-	#end
 end
 
 proxy "/chapters/index.html", "/templates/chapters.html", :locals => { :chapters => chapters }, :ignore => true
