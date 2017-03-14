5b6afe808db64a027c7915e1f36a728356ad4c95
Eric Weber <eric@weber.lol>
2017-03-14 08:21:19 -0700

    load books and associated media managers from yaml file

--- gallaudet.rb
+++ gallaudet.rb
@@ -62,14 +62,9 @@ def add_media_to_chapter(media, media_manager_name, chapters, others)
   end
 end
 
+INPUT_DATA_PATH = "."
+BOOKS_INPUT_FILE = "books.yaml"
 OUTPUT_DATA_PATH = "data"
-MEDIA_MANAGERS = [
-  {"name" => "burgess", "processing_type" => "plain", "content_type" => "passages", "content_path" => "passages", "content_name" => "Passages", "input_data_path" => "burgess_data", "input_data_file" => "passages.yaml", "output_filename" => "burgess_passages.json"},
-  {"name" => "macchi", "processing_type" => "plain", "content_type" => "movies", "content_path" => "movies", "content_name" => "Movies", "input_data_path" => "macchi_data", "input_data_file" => "movies.yaml", "output_filename" => "macchi_movies.json"},
-  {"name" => "bloch", "processing_type" => "plain", "content_type" => "slideshows", "content_path" => "slideshows", "content_name" => "Slide Shows", "input_data_path" => "bloch_data", "input_data_file" => "slideshows.yaml", "output_filename" => "bloch_slideshows.json"},
-  {"name" => "hawker", "processing_type" => "plain", "content_type" => "tours", "content_path" => "tours", "content_name" => "Tours", "input_data_path" => "hawker_data", "input_data_file" => "tours.yaml", "output_filename" => "hawker_tours.json"},
-  {"name" => "ikarus", "processing_type" => "galleries", "content_type" => "galleries", "input_data_path" => "ikarus_data", "input_data_file" => "galleries.yaml", "input_data_entries_path" => "galleries", "output_filename" => "ikarus_galleries.json"}
-]
 
 GALLAUDET_DATA_PATH = "."
 GALLAUDET_CHAPTERS_FILENAME = "gallaudet_chapters.yaml"
@@ -77,21 +72,28 @@ GALLAUDET_OTHERS_FILENAME = "gallaudet_others.yaml"
 GALLAUDET_CHAPTERS_OUTPUT_FILENAME = "gallaudet_chapters.json"
 GALLAUDET_OTHERS_OUTPUT_FILENAME = "gallaudet_others.json"
 
+books = YAML.load_file("#{INPUT_DATA_PATH}/#{BOOKS_INPUT_FILE}")
 
-chapter_data_path = "clawing"
-gallaudet_chapters_output_file = File.open("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{GALLAUDET_CHAPTERS_OUTPUT_FILENAME}", "w")
-gallaudet_others_output_file = File.open("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{GALLAUDET_OTHERS_OUTPUT_FILENAME}", "w")
-gallaudet_chapters = YAML.load_file("#{GALLAUDET_DATA_PATH}/#{GALLAUDET_CHAPTERS_FILENAME}")
-gallaudet_others = []
+books.each do |book|
+  chapter_data_path = book["shortname"]
+  media_managers = book["media_managers"]
 
-MEDIA_MANAGERS.each do |media_manager|
-  process_plain_media(media_manager, chapter_data_path) if media_manager["processing_type"] == "plain"
-  process_gallery_media(media_manager, chapter_data_path) if media_manager["processing_type"] == "galleries"
+  gallaudet_chapters_output_file = File.open("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{GALLAUDET_CHAPTERS_OUTPUT_FILENAME}", "w")
+  gallaudet_others_output_file = File.open("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{GALLAUDET_OTHERS_OUTPUT_FILENAME}", "w")
+  gallaudet_chapters = YAML.load_file("#{GALLAUDET_DATA_PATH}/#{GALLAUDET_CHAPTERS_FILENAME}")
+  gallaudet_others = []
 
-  processing_output_file = JSON.parse(File.read("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{media_manager["output_filename"]}"))
-  add_media_to_chapter(processing_output_file, media_manager["name"], gallaudet_chapters, gallaudet_others)
+  if media_managers != nil
+    media_managers.each do |media_manager|
+      process_plain_media(media_manager, chapter_data_path) if media_manager["processing_type"] == "plain"
+      process_gallery_media(media_manager, chapter_data_path) if media_manager["processing_type"] == "galleries"
+
+      processing_output_file = JSON.parse(File.read("#{OUTPUT_DATA_PATH}/#{chapter_data_path}/#{media_manager["output_filename"]}"))
+      add_media_to_chapter(processing_output_file, media_manager["name"], gallaudet_chapters, gallaudet_others)
+    end
+  end
+
+  puts JSON.pretty_generate(gallaudet_chapters) if GALLAUDET_VERBOSE
+  gallaudet_chapters_output_file.write(JSON.pretty_generate(gallaudet_chapters))
+  gallaudet_others_output_file.write(JSON.pretty_generate(gallaudet_others))
 end
-
-puts JSON.pretty_generate(gallaudet_chapters) if GALLAUDET_VERBOSE
-gallaudet_chapters_output_file.write(JSON.pretty_generate(gallaudet_chapters))
-gallaudet_others_output_file.write(JSON.pretty_generate(gallaudet_others))
