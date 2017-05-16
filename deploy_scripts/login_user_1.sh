c33661c70ddd88f7a781d98ff3b1ee9315dca195
Eric Weber <eric@weber.lol>
2017-05-16 15:41:25 +0000

    add developer login scripts with git configs

---
+++ deploy_scripts/login_user_1.sh
@@ -0,0 +1,3 @@
+git config --global user.name "$USER_1_NAME"
+git config --global user.email "$USER_1_EMAIL"
+echo $USER_1_NAME logged in
---
+++ deploy_scripts/login_user_2.sh
@@ -0,0 +1,3 @@
+git config --global user.name "$USER_2_NAME"
+git config --global user.email "$USER_2_EMAIL"
+echo $USER_2_NAME logged in
