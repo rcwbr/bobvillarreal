6b95ae6d4139832fce5671c224909d2df3ff961c
Eric Weber <eric@weber.lol>
2017-07-14 08:16:32 -0700

    add support for sectioned galleries in ikarus script

--- input_scripts/ikarus.rb
+++ input_scripts/ikarus.rb
@@ -20,64 +20,75 @@ IMAGE_DEFAULTS = {
   "attribution" => ""
 }
 
-def ikarus_init(ikarus_data_path, ikarus_output_filename, galleries_data_file, galleries_data_path, book_image_path, media_manager)
+def generate_gallery(ikarus_data_path, galleries_info, gallery_counter, galleries_data_path, book_image_path, media_manager)
+  if galleries_info[gallery_counter]["shortname"] == nil
+    galleries_info[gallery_counter]["shortname"] = galleries_info[gallery_counter]["name"]
+  end
+  if galleries_info[gallery_counter]["images_path"] == nil
+    galleries_info[gallery_counter]["images_path"] = "#{GALLERY_DEFAULTS["images_path"]}/#{book_image_path}/#{GALLERY_DEFAULTS["galleries_path"]}/#{galleries_info[gallery_counter]["path"]}"
+  end
 
+  for gallery_defaults_counter in 1...GALLERY_DEFAULTS.keys.length
+    if galleries_info[gallery_counter][GALLERY_DEFAULTS.keys[gallery_defaults_counter]] == nil
+      galleries_info[gallery_counter][GALLERY_DEFAULTS.keys[gallery_defaults_counter]] = GALLERY_DEFAULTS[GALLERY_DEFAULTS.keys[gallery_defaults_counter]]
+    end
+  end
+
+  puts "IKARUS: #{galleries_info[gallery_counter]["name"]} | #{galleries_info[gallery_counter]["shortname"]} | #{galleries_info[gallery_counter]["source_path"]} | #{galleries_info[gallery_counter]["images_path"]} | #{galleries_info[gallery_counter]["thumbs_folder_path"]} | #{galleries_info[gallery_counter]["total_width"]} | #{galleries_info[gallery_counter]["images_per_row"]} | #{galleries_info[gallery_counter]["image_margins"]} | #{galleries_info[gallery_counter]["width_adjustment"]}" if IKARUS_VERBOSE
+
+  galleries_info[gallery_counter]["image_matrix"] = []
+
+  gallery_images_info = YAML.load_file("#{ikarus_data_path}/#{galleries_data_path}/#{galleries_info[gallery_counter]["filename"]}")
+  label_counter = 1
+  for image_counter in 0...gallery_images_info.length
+    for images_defaults_counter in 0...IMAGE_DEFAULTS.length
+      if gallery_images_info[image_counter][IMAGE_DEFAULTS.keys[images_defaults_counter]] == nil
+        gallery_images_info[image_counter][IMAGE_DEFAULTS.keys[images_defaults_counter]] = IMAGE_DEFAULTS[IMAGE_DEFAULTS.keys[images_defaults_counter]]
+      end
+    end
+    if gallery_images_info[image_counter]["label"] == nil
+      gallery_images_info[image_counter]["label"] = label_counter.to_s
+      label_counter += 1
+    end
+  end
+
+  curtiss_init(galleries_info[gallery_counter]["image_matrix"], gallery_images_info, "#{galleries_info[gallery_counter]["source_path"]}/#{galleries_info[gallery_counter]["images_path"]}", galleries_info[gallery_counter]["thumbs_folder_path"], galleries_info[gallery_counter]["total_width"], galleries_info[gallery_counter]["images_per_row"], galleries_info[gallery_counter]["image_margins"], galleries_info[gallery_counter]["width_adjustment"])
+
+  for i in 0...galleries_info[gallery_counter]["image_matrix"].length
+    print "IKARUS: " if IKARUS_VERBOSE
+    for j in 0...galleries_info[gallery_counter]["image_matrix"][i].length
+      print "#{galleries_info[gallery_counter]["image_matrix"][i][j]} " if IKARUS_VERBOSE
+    end
+    puts "" if IKARUS_VERBOSE
+  end
+
+  galleries_info[gallery_counter]["content_name"] = media_manager["content_name"]
+  galleries_info[gallery_counter]["content_path"] = media_manager["content_path"]
+  galleries_info[gallery_counter]["page_title"] = media_manager["page_title"]
+  if media_manager["content_longname"]
+    galleries_info[gallery_counter]["content_longname"] = media_manager["content_longname"]
+  end
+  if media_manager["entry_page_title"]
+    galleries_info[gallery_counter]["entry_page_title"] = media_manager["entry_page_title"]
+  else
+    galleries_info[gallery_counter]["entry_page_title"] = media_manager["page_title"]
+  end
+end
+
+def ikarus_init(ikarus_data_path, ikarus_output_filename, galleries_data_file, galleries_data_path, book_image_path, media_manager)
   galleries_info = YAML.load_file("#{ikarus_data_path}/#{galleries_data_file}")
   galleries_json_file = File.open(ikarus_output_filename, "w")
 
-  for gallery_counter in 0...galleries_info.length
-
-    if galleries_info[gallery_counter]["shortname"] == nil
-      galleries_info[gallery_counter]["shortname"] = galleries_info[gallery_counter]["name"]
-    end
-    if galleries_info[gallery_counter]["images_path"] == nil
-      galleries_info[gallery_counter]["images_path"] = "#{GALLERY_DEFAULTS["images_path"]}/#{book_image_path}/#{GALLERY_DEFAULTS["galleries_path"]}/#{galleries_info[gallery_counter]["path"]}"
-    end
-
-    for gallery_defaults_counter in 1...GALLERY_DEFAULTS.keys.length
-      if galleries_info[gallery_counter][GALLERY_DEFAULTS.keys[gallery_defaults_counter]] == nil
-        galleries_info[gallery_counter][GALLERY_DEFAULTS.keys[gallery_defaults_counter]] = GALLERY_DEFAULTS[GALLERY_DEFAULTS.keys[gallery_defaults_counter]]
+  if media_manager["sectioned"]
+    for gallery_section_counter in 0...galleries_info.length
+      galleries_info[gallery_section_counter]["section"] = true
+      for gallery_counter in 0...galleries_info[gallery_section_counter]["galleries"].length
+        generate_gallery(ikarus_data_path, galleries_info[gallery_section_counter]["galleries"], gallery_counter, galleries_data_path, book_image_path, media_manager)
       end
     end
-
-    puts "IKARUS: #{galleries_info[gallery_counter]["name"]} | #{galleries_info[gallery_counter]["shortname"]} | #{galleries_info[gallery_counter]["source_path"]} | #{galleries_info[gallery_counter]["images_path"]} | #{galleries_info[gallery_counter]["thumbs_folder_path"]} | #{galleries_info[gallery_counter]["total_width"]} | #{galleries_info[gallery_counter]["images_per_row"]} | #{galleries_info[gallery_counter]["image_margins"]} | #{galleries_info[gallery_counter]["width_adjustment"]}" if IKARUS_VERBOSE
-
-    galleries_info[gallery_counter]["image_matrix"] = []
-
-    gallery_images_info = YAML.load_file("#{ikarus_data_path}/#{galleries_data_path}/#{galleries_info[gallery_counter]["filename"]}")
-    label_counter = 1
-    for image_counter in 0...gallery_images_info.length
-      for images_defaults_counter in 0...IMAGE_DEFAULTS.length
-        if gallery_images_info[image_counter][IMAGE_DEFAULTS.keys[images_defaults_counter]] == nil
-          gallery_images_info[image_counter][IMAGE_DEFAULTS.keys[images_defaults_counter]] = IMAGE_DEFAULTS[IMAGE_DEFAULTS.keys[images_defaults_counter]]
-        end
-      end
-      if gallery_images_info[image_counter]["label"] == nil
-        gallery_images_info[image_counter]["label"] = label_counter.to_s
-        label_counter += 1
-      end
-    end
-
-    curtiss_init(galleries_info[gallery_counter]["image_matrix"], gallery_images_info, "#{galleries_info[gallery_counter]["source_path"]}/#{galleries_info[gallery_counter]["images_path"]}", galleries_info[gallery_counter]["thumbs_folder_path"], galleries_info[gallery_counter]["total_width"], galleries_info[gallery_counter]["images_per_row"], galleries_info[gallery_counter]["image_margins"], galleries_info[gallery_counter]["width_adjustment"])
-
-    for i in 0...galleries_info[gallery_counter]["image_matrix"].length
-      print "IKARUS: " if IKARUS_VERBOSE
-      for j in 0...galleries_info[gallery_counter]["image_matrix"][i].length
-        print "#{galleries_info[gallery_counter]["image_matrix"][i][j]} " if IKARUS_VERBOSE
-      end
-      puts "" if IKARUS_VERBOSE
-    end
-
-    galleries_info[gallery_counter]["content_name"] = media_manager["content_name"]
-    galleries_info[gallery_counter]["content_path"] = media_manager["content_path"]
-    galleries_info[gallery_counter]["page_title"] = media_manager["page_title"]
-    if media_manager["content_longname"]
-      galleries_info[gallery_counter]["content_longname"] = media_manager["content_longname"]
-    end
-    if media_manager["entry_page_title"]
-      galleries_info[gallery_counter]["entry_page_title"] = media_manager["entry_page_title"]
-    else
-      galleries_info[gallery_counter]["entry_page_title"] = media_manager["page_title"]
+  else
+    for gallery_counter in 0...galleries_info.length
+      generate_gallery(ikarus_data_path, galleries_info, gallery_counter, galleries_data_path, book_image_path, media_manager)
     end
   end
 
