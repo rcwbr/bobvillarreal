07edb588a32bd734f28f96fbfc17b3c3ad5c08fd
Eric Weber <eric@weber.lol>
2017-10-11 22:46:36 -0700

    fix index out of bounds bug in curtiss image processing

--- input_scripts/curtiss_image_process.rb
+++ input_scripts/curtiss_image_process.rb
@@ -1,4 +1,5 @@
 require "rmagick" #rmagick is the library used to access and edit image files. It defines Magick::ImageList used below.
+require "json"
 CURTISS_VERBOSE = false
 
 # curtiss_init iterates through the files in gallery_folder, images_per_row at a time. For each set of images_per_row images, it finds the minimum height of the images, and adds up the width of each image if it were scaled to the minimum height. If this width is less than total_width - width_adjustment - image_margins * (the number of images in the row - 1), it then adds another image to the list. It repeats this until the total reaches total_width - width_adjustment, at which point it scales each image by a factor of (total_width - width_adjustment - image_margins * (the number of images in the row - 1)) / the added width of the images (as if scaled to minimum height), saves the result, and pushes the filenames to a row of the gallery matrix. When it reaches the point where less than images_per_row filenames are left, it scales the remaining images, if any, such that they are the same height as each other and fill the same width as the other rows.
@@ -25,7 +26,7 @@ def curtiss_init(gallery, images_info, gallery_folder, thumbs_folder, total_widt
     end
 
     imagesToAdd = 0
-    while width < total_width
+    while width < total_width and (index + images_per_row + imagesToAdd) < images_info.length
       imagesToAdd += 1
       image = images_info[index + images_per_row + imagesToAdd]
       currentImage = Magick::ImageList.new(gallery_folder + "/" + image["filename"])
